c6fb70858d7d3530f646a151a918e347857a76f5
Matthias Plank <matthias.plank@gmx.at>
2024-02-02 22:11:18 +0100

    add html

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from "./sidenav/sidenav.component";
 import { HeaderComponent } from "./header/header.component";
+import { SummaryComponent } from "./summary/summary.component";
 
 @Component({
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss',
-    imports: [SidenavComponent, HeaderComponent]
+    imports: [SidenavComponent, HeaderComponent, SummaryComponent]
 })
 export class MainComponent {
 
